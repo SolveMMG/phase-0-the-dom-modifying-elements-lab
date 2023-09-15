51916ea704fef0032e1f308c1a05b3d9a040ccd2
Your Name <mikachupichu@gmail.com>
2023-09-15 18:01:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main1 = document.querySelector('main#main');
+  main1.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Michael Thomas is the champion";
+
+
